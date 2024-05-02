1449cb6d4749e94dc81cc72edfed900e7a095a97
skkcoder <165959722+skkcoder@users.noreply.github.com>
2024-05-02 21:33:18 +0100

    Initial files for the demo application

---
+++ src/test/kotlin/com/skk/demo/jooq/SpringBootJooqApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.skk.demo.jooq
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringBootJooqApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
